6da0d4a014a4a9b53ef23365cb0de56e38d20aef
PrabhudevMishra <70210156+PrabhudevMishra@users.noreply.github.com>
2020-09-19 23:45:17 +0530

    Update Dustbin.js

--- Dustbin.js
+++ Dustbin.js
@@ -1,7 +1,12 @@
 class Dustbin {
-    constructer(x, y, width, height){
-
-    this.body = Bodies.rectangle(x, y, width, height);
+    constructor(x, y, width, height){
+    var options = {
+      isStatic:true,
+		  restituition:0.3,
+		  friction:0.5,
+		  density: 1.2
+      }
+    this.body = Bodies.rectangle(0, 0, width, height,options);
     this.width = width;
     this.height = height;
 
