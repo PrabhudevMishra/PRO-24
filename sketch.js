a6650a16efda47032126fd40bf3625488d4e1801
PrabhudevMishra <70210156+PrabhudevMishra@users.noreply.github.com>
2020-09-19 23:47:23 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,46 +3,39 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var dust1,dust2,dust3;
-
-
-//function preload()
-//{
-	
-//}
+var dust1,dust2,dust3,paper,ground;
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(800, 600);
 
 	engine = Engine.create();
 	world = engine.world;
 
-	var options = {
-		isStatic:true,
-		restituition:0.3,
-		friction:0.5,
-		density: 1.2
-	}
-
 	//Create the Bodies Here.
-	dust1 = new Dustbin(300,100,20,20);
-	paperObject = Bodies.circle(20,20,5,(options));
-
-	//Engine.run(engine);
+	ground = new Ground(600, 580, 1200, 20);
+	paper = new PaperObject(100, 100, 10);
+	dust1 = new Dustbin(800, 320, 20, 100);
+	dust2 = new Dustbin(1000, 320, 20, 100);
+	dust3 = new Dustbin(300, 300, 20, 20);
+    //Engine.run(engine);
 }
 
 
 function draw() {
-  rectMode(CENTER);
+  //rectMode(CENTER);
   background(0);
   Engine.update(engine);
- 
+
+  ground.display();
   dust1.display();
+  dust2.display();
+  dust3.display();
+  paper.display();
 }
 
 function keyPressed(){
 	if(keyCode === UP_ARROW){
-		Matter.body.applyForce(paperObject.body,paperObject.body.position,{x:85, y:85});
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:10, y:-20});
 	}
 }
 
